9ef8d349d519a3e1876922d29e28b77f28944d17
Barack Obama <98446337+Limeed@users.noreply.github.com>
2024-05-16 22:00:16 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    
+    // Fetch the file contents
+    fetch('subject.txt')
+        .then(response => response.text())
+        .then(text => {
+            // Display the file contents
+            console.log(text);
+        })
+        .catch(error => {
+            console.error('Error fetching subject.txt:', error);
+        });
+});
+
+
+function getRandomItem(list) {
+
+    const randomIndex = Math.floor(Math.random() * list.length);
+
+    return list[randomIndex];
+  }
+
+function createNames() {
+    
+
+    
+    //let randomItem = getRandomItem(textList);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //document.getElementById("names").textContent="newtext"; 
+}
